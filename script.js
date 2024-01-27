00db2ab309162682f0f1d7b587bd545bdb027bae
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-01-27 16:49:28 +0000

    improved nav bar

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let lastScrollPosition = 0;
 let scrollingTimer;
 
-const topNav = document.getElementById("desktop-nav");
+const topNav = document.getElementById("menu-icon");
 
 window.addEventListener("scroll", () => {
   clearTimeout(scrollingTimer);
@@ -26,9 +26,42 @@ window.addEventListener("scroll", () => {
   });
 });
 
+// function toggleMenu() {
+//   const menu = document.querySelector(".menu-links");
+//   const icon = document.querySelector(".hamburger-icon");
+//   menu.classList.toggle("open");
+//   icon.classList.toggle("open");
+// }
+
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  var menu = document.getElementById("hamburger-menu");
+  var mainContent = document.getElementById("main-content");
+  if (menu.style.left === "-100%") {
+    menu.style.left = "0";
+    mainContent.classList.add("pushed");
+    // Add event listener to close menu when clicking outside
+    document.addEventListener("click", closeMenuOutside);
+  } else {
+    menu.style.left = "-100%";
+    mainContent.classList.remove("pushed");
+    // Remove event listener when menu is closed
+    document.removeEventListener("click", closeMenuOutside);
+  }
+}
+
+function closeMenu() {
+  var menu = document.getElementById("hamburger-menu");
+  var mainContent = document.getElementById("main-content");
+  menu.style.left = "-100%";
+  mainContent.classList.remove("pushed");
+  // Remove event listener when menu is closed
+  document.removeEventListener("click", closeMenuOutside);
+}
+
+function closeMenuOutside(event) {
+  var menu = document.getElementById("hamburger-menu");
+  var menuIcon = document.getElementById("menu-icon");
+  if (!menu.contains(event.target) && event.target !== menuIcon) {
+    closeMenu();
+  }
 }
